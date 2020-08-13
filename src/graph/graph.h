b3037a584e7c93c0b960313372d428069679ca78
s267337 <69515162+s267337@users.noreply.github.com>
2020-08-13 22:47:52 +0800

    Update graph.h

--- src/graph/graph.h
+++ src/graph/graph.h
@@ -1,16 +1,59 @@
-#ifndef GRAPH_HEADER
-#define GRAPH_HEADER
+//
+//  graph.h
+//  project
+//
+//  Created by 郭雅丹 on 2020/8/13.
+//  Copyright © 2020 GUO YADAN. All rights reserved.
+//
 
-struct graph {
+#ifndef graph_h
+#define graph_h
 
+#include <stdio.h>
+
+
+
+#define MAX_LINE 100
+enum {WHITE, GREY, BLACK};
+
+
+typedef struct graph_s graph_t;
+typedef struct vertex_s vertex_t;
+typedef struct edge_s edge_t;
+ 
+struct graph_s {
+    vertex_t *g;
+    unsigned int nv;
 };
 
-typedef struct graph graph_t;
+struct vertex_s {
+    unsigned int id;
+    int color;
+    int dist;
+    int disc_time;
+    int endp_time;
+    int pred;
+    int scc;
+    int *rowAdj;
+};
 
-int graph_init(graph_t*);
+struct edge_s {
+    int weight;
+    vertex_t *dst;
+    edge_t *next;
+};
 
 
 
-int graph_destroy(graph_t*);
 
-#endif // GRAPH_HEADER
+graph_t* graph_load(char*);
+void graph_attribute_init (graph_t*);
+int graph_find(graph_t*,int);
+void graph_dispose(graph_t*);
+
+
+
+
+
+
+#endif /* graph_h */
