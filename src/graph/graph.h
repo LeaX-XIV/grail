3f57c093df7fc9c9541be5abc77150e814cfc5ab
Alessandro Borione <leax_xiv@me.com>
2020-08-13 14:20:12 +0200

    Add skeleton of list and graph modules

---
+++ src/graph/graph.h
@@ -0,0 +1,16 @@
+#ifndef GRAPH_HEADER
+#define GRAPH_HEADER
+
+struct graph {
+
+};
+
+typedef struct graph graph_t;
+
+int graph_init(graph_t*);
+
+
+
+int graph_destroy(graph_t*);
+
+#endif // GRAPH_HEADER
---
+++ src/list/list.h
@@ -0,0 +1,16 @@
+#ifndef LIST_HEADER
+#define LIST_HEADER
+
+struct list {
+
+};
+
+typedef struct list list_t;
+
+int list_init(list_t*);
+
+
+
+int list_destroy(list_t*);
+
+#endif // LIST_HEADER
