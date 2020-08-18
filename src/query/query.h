8134322b044d9fbdf125f86eb321f6b3103ac27e
s267337 <69515162+s267337@users.noreply.github.com>
2020-08-18 23:02:55 +0800

    Update query.h

--- src/query/query.h
+++ src/query/query.h
@@ -1,10 +1,3 @@
-//
-//  query.h
-//  project
-//
-//  Created by 郭雅丹 on 2020/8/18.
-//  Copyright © 2020 GUO YADAN. All rights reserved.
-//
 
 #ifndef query_h
 #define query_h
@@ -21,10 +14,5 @@ struct thread_data{
 static void* filereadthread(void *arg);
 static void* querythread(void *arg);
 int query(int u,int v,graph_t*g,int d);
-
-
-
-
-
 
 #endif /* query_h */
