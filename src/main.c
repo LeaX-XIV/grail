5914d31eec69609269a1efe192940367762bf7b6
s267337 <69515162+s267337@users.noreply.github.com>
2020-08-18 22:40:42 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -22,35 +22,36 @@ int main(int argc, const char * argv[]) {
     d=atoi(argv[1]);
     graph_t * g;
     
-    g=graph_load(argv[0],d);
-    
-    //do the labelling
-    
-    FILE *fp;
-    
-    pthread_mutex_init(&mutex,NULL);
-    
-    fp=fopen(argv[2], "r");
-    
     void* status;
     int retcode;
     pthread_t th[4];
     struct file_data f;
+    
+    FILE *fp;
+    
+    g=graph_load(argv[0],d);
+    
+    //do the labelling
+    
+        
+    pthread_mutex_init(&mutex,NULL);
+    
+    fp=fopen(argv[2], "r");
     
     for(int i=0;i<4;i++){
         
       retcode=pthread_create(&th[i], NULL, filereadthread, &f);
       if (retcode != 0)
           fprintf (stderr, "create GET failed in main %d\n", retcode);
-      
     }
     
     for(int j=0;j<4;j++){
-        
          pthread_join(th[j],&status);
-        
     }
 
+    printf("query finish..\n");
     pthread_mutex_destroy(&mutex);
+    fclose(fp);
     return 0;
 }
+
