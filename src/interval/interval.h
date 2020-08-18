1c29977a58dd381e8f56955533c0c1eba544d70b
s267337 <69515162+s267337@users.noreply.github.com>
2020-08-18 23:03:52 +0800

    Update interval.h

--- src/interval/interval.h
+++ src/interval/interval.h
@@ -1,10 +1,3 @@
-//
-//  interval.h
-//  project
-//
-//  Created by 郭雅丹 on 2020/8/14.
-//  Copyright © 2020 GUO YADAN. All rights reserved.
-//
 
 #ifndef INTERVAL_HEADER
 #define INTERVAL_HEADER
@@ -16,10 +9,10 @@ struct interval{
 
 typedef struct interval interval_t;
 
-interval_t* interval_create(int first, int second);
-int interval_first(interval_t* i);
-int interval_second(interval_t* i);
-int interval_check(interval_t* i1, interval_t* i2);
-void interval_destroy(interval_t* i);
+interval_t* interval_create(int , int );
+int interval_first(interval_t* );
+int interval_second(interval_t* );
+int interval_check(interval_t* , interval_t*);
+void interval_destroy(interval_t* );
 
 #endif // INTERVAL_HEADER
